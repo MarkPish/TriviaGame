b106de3817e1d5a58dc41f0478e0d20f45d711b6
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-08-26 14:03:26 -0400

    image correction

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+
+var questions = [{
+    question: "The capital of Italy is ?",
+    choices: ["Florence", "Milan", "Rome", "Calabria", "Palermo"],
+    correctAnswer: 2
+}, {
+    question: "The Italian city of Portofino is located on ?",
+    choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
+    correctAnswer: 4
+}, {
+    // question: "The Italian city of Portofino is located on ?",
+    choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
+    correctAnswer: 4
+}, {
+    question: "The Italian city of Portofino is located on ?",
+    choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
+    correctAnswer: 4
+}, {
+    question: "The Italian city of Portofino is located on ?",
+    choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
+    correctAnswer: 4
+}, {
+    question: "The Italian city of Portofino is located on ?",
+    choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
+    correctAnswer: 4 
+}, {
+    question: "The Italian city of Portofino is located on ?",
+    choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
+    correctAnswer: 4 
+}, {
+    question: "The Italian city of Portofino is located on ?",
+    choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
+    correctAnswer: 4
+}, {
+    question: "The Italian city of Portofino is located on ?",
+    choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
+    correctAnswer: 4
+}, {
+    question: "The Italian city of Portofino is located on ?",
+    choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
+    correctAnswer: 4
+}
+]
