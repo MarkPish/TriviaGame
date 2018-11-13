5c7cc0507bc5e023879941f42f3d68224ab0fe06
Mark Piscioneri <markjpiscioneri77@gmail.com>
2018-11-13 11:17:06 -0500

    added more animals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,72 +2,67 @@
 var questions = [{
     question: "The capital of Italy is ?",
     choices: ["Florence", "Milan", "Rome", "Calabria", "Palermo"],
-    correctAnswer: 2
-    slide1a.jpg
-}, {
+    correctAnswer: 2 },
+    // slide1a.jpg
+ {
     question: "The Italian city of Portofino is located on ?",
     choices: ["the Amalfi coast", "the Strait of Messina", "the Adriatic", "Lake Cuomo", "the Italian Riviera"],
-    correctAnswer: 4
+    correctAnswer: 4 },
     //slide2a.jpg
-}, {
+ {
     question: "Which country, besides Vatican City, is also land-locked within Italy?",
     choices: ["Sardinia", "Capri", "Montenegro", "San Marino", "Moldova"],
-    correctAnswer: 3
+    correctAnswer: 3 },
     //slide3a.jpg
-}, {
+ {
     question: "All of the following are rivers in Italy, except?",
     choices: [" the Po", "the Rubicon", "the Tiber", "the Arno", "the Rhine"],
-    correctAnswer: 4
+    correctAnswer: 4 },
     //slide4a.jpg
-}, {
+ {
     question: "A traditional Caprisi salad mimics the Italian flag in color, having 3 main ingredients; tomatoes, basil and this type of fresh cheese?",
     choices: ["Mozzarella", "Fontina", "Goat", "Cheddar", "Gorganzola"],
-    correctAnswer: 0
+    correctAnswer: 0 },
     //slide5a.jpg
-}, {
+ {
     question: "Which Italian city is home to the exotic car maker, Ferrari?",
     choices: ["Naples", "Pirelli", "Maranello", "Turin", "Florence"],
-    correctAnswer: 2
+    correctAnswer: 2 },
     //slide6a.jpg
-}, {
+ {
     question: "Gelato, an Italian ice cream, boasts this many gelaterias (stores) selling it in Italy?",
     choices: ["6,000", "19,000", "26,000", "32,000", "39,000"],
-    correctAnswer: 4
+    correctAnswer: 4 },
     //slide7a.jpg 
-}, {
+ {
     question: "Which of the following wine grape is NOT grown in Italy?",
     choices: ["Barbera", "Sangiovese", "Chianti", "Bordeaux", "Merlot"],
-    correctAnswer: 3
+    correctAnswer: 3 },
     //slide8a.jpg
-}, {
+ {
     question: "In 79 A.D., Mount Vesuvius, near Naples, erupted, burying the ancient city of Pompeii under thick volcanic ash. What other city was also buried?",
     choices: ["Verona", "Herculaneum", "Aleppo", "Torino", "Rome"],
-    correctAnswer: 1
+    correctAnswer: 1 },
     //slide9a.jpg
-}, {
+ {
     question: "As of 2015, how many scooters are registered in Italy?",
     choices: ["950,000", "1.9 million", "3.6 million", "5.1 million", "7.4 million"],
-    correctAnswer: 2
+    correctAnswer: 2 },
     //slide10a.jpg
-}, {
-    question: "Which mountain range is located in central Italy?",
-    choices: ["Italian Alps", "Himalayas", "Dolomites", "Apennines", "Pyrenees"],
-    correctAnswer: 3
+ {
+    question: "Which mountain range is located in northern Italy?",
+    choices: ["Urals", "Himalayas", "Dolomites", "Apennines", "Pyrenees"],
+    correctAnswer: 2 },
     //slide11a.jpg
-}, {
+ {
     question: "Which Italian city is home to a new sculpture, depicting gigantic arms and hands rising from the water?",
     choices: ["Rome", "Milan", "Pisa", "Venice", "Palermo"],
-    correctAnswer: 3
+    correctAnswer: 3 },
     //slide12a.jpg
-}
-];
-// var questions = questions in quiz
-// var questionCounter = question number
-// var selections = player's answer
-// var
 
-// Tracks question number
-var questionCounter = 0; 
+];
+
+var questionNum = 0; 
 var selections = [];
 var quiz = $('#quiz');
 
@@ -76,8 +71,8 @@ var images = ["assets/images/slide1a.jpg", "assets/images/slide2a.jpg", "assets/
 var showImage;
 var count = 0;
 
-// $("#start").click(startSlideshow);
-// $("#stop").click(stopSlideshow);
+$("#start").click(startSlideshow);
+$("#stop").click(stopSlideshow);
 
 function displayImage() {
     $("#image-holder").html("<img src=" + images[count] + " width='400px'>");
@@ -90,15 +85,12 @@ function nextImage() {
     }
 }
 function startSlideshow() {
-    showImage = setInterval(nextImage, 8000);
-
+    showImage = setInterval(nextImage, 10000);
 }
 function stopSlideshow() {
-    clearInterval(showImage); //check here for problem with slideshow not reseting...eliminated buttons...need to set to questions...next.onclick
-
+    clearInterval(showImage);
 }
 displayImage();
-
 // Display first question
 displayNext();
 timer();
@@ -108,16 +100,14 @@ $('#done').hide();
 $('#next').on('click', function(){
     choose();
     // if they don't pick an answer set up alert
-    if (isNaN(selections[questionCounter])) {
-        alert("Please make a selection!");
+    if (isNaN(selections[questionNum])) {
+        alert("Please make a selection!!!");
     }
     else {
-        questionCounter++;
+        questionNum++;
         displayNext();
-
     }
 });
-
 // sets the timer
 function timer() {
     t = setTimeout(timeUp, 1000 * 120);
@@ -165,7 +155,7 @@ function timeUp() {
     // Need to reset game when the 'Start Over' button is clicked, reset all counters! hide and show appropriate buttons
 
     $('#start').on('click', function() {
-        questionCounter = 0;
+        questionNum = 0;
         selections = [];
         displayNext();
         $('#start').hide();
@@ -177,7 +167,7 @@ function timeUp() {
         countDown();
     });
 
-    // Need to create question and choosen answer divs
+    // Need to create question and chosen answer divs
 
     function makeQuiz(index) {
         var quizDiv = $('<div>', {
@@ -201,30 +191,30 @@ function timeUp() {
          for (var i = 0; i < questions[index].choices.length; i++) {
              var item = $('<li>');     // creating each list item
              input = '<input type="radio" name="answer" value=' + i + ' />';
-             input += questions[index].choices[i];     //input is equal to input plus
+             input += questions[index].choices[i];     
              questions[index].choices[i]
-             item.append(input);    //adding the list to the space created
-             radioList.append(item);     //adding it to the radio list
+             item.append(input);   
+             radioList.append(item);    
          }
          return radioList;
      }
 
-     //Reads the user selection and pushes the value to an array
+     
      function choose() {
-         selections[questionCounter] = +$('input[name="answer"]:checked').val();
+         selections[questionNum] = +$('input[name="answer"]:checked').val();
      }
 
      //Displays next requested element.... research .attr or .prop
      function displayNext() {
          quiz.fadeOut(function() {
              $('#question').remove();
-             if (questionCounter < questions.length) {
-                 var nextQuestion = makeQuiz(questionCounter);
+             if (questionNum < questions.length) {
+                 var nextQuestion = makeQuiz(questionNum);
                  quiz.append(nextQuestion).show();
-                 if (!(isNaN(selections[questionCounter]))) {
-                     $('input[value='+selections[questionCounter]+']').prop('checked', true);
+                 if (!(isNaN(selections[questionNum]))) {
+                     $('input[value='+selections[questionNum]+']').prop('checked', true);
                  }
-                 else if (questionCounter === 0) {
+                 else if (questionNum === 0) {
                      $('#next').show();
                     }
                 }
@@ -237,7 +227,7 @@ function timeUp() {
         });
      };
 
-     //computes score and returns a paragraph element to be displayed
+     //compute score and return a paragraph element to be displayed
      function displayScore() {
          clearTimeout(t);
          $('#time').hide();
